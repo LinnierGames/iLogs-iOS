797efc9ed31b567f8f755f593a012b35a58dfb99
Erick <esericksanc@gmail.com>
2015-06-15 17:05:59 -0700

    init Diary ViewControllers
    DiaryViewController
    DiaryMapViewController
    SettingsViewController

---
+++ iLog/DIaryMapViewController.h
@@ -0,0 +1,13 @@
+//
+//  DIaryMapViewController.h
+//  iLog
+//
+//  Created by Erick Sanchez on 6/15/15.
+//  Copyright (c) 2015 Erick Sanchez. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DIaryMapViewController : UIViewController
+
+@end
