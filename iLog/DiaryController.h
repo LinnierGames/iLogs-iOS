c60783a5d6986880da5d2eda158e5ca1bd5db64e
Erick <esericksanc@gmail.com>
2015-06-16 23:33:40 -0700

    init UniversalFunctions (SQL_Entries_)

--- iLog/DiaryController.h
+++ iLog/DiaryController.h
@@ -73,3 +73,81 @@ static inline NSMutableArray * SQLStatementRowIntoDiaryEntry( sqlite3_stmt *stat
 + (void)SQL_DIARIES_voidDeleteRowWithArray:(const NSArray *)arrayEntry;
 
 @end
+
+static const NSUInteger ENTRIES_subject;
+static const NSUInteger ENTRIES_body;
+static const NSUInteger ENTRIES_hasImage;
+static const NSUInteger ENTRIES_hasAudioMemo;
+static const NSUInteger ENTRIES_isBookmarked;
+static const NSUInteger ENTRIES_date;
+static const NSUInteger ENTRIES_dateCreated;
+
+@interface NSArray (ARRAY_Entries_)
+
++ (id)arrayNEWEntry;
++ (id)arrayNEWEntryWithSubject:(NSString *)stringSubjectValue body:(NSString *)stringBodyValue;
++ (id)arrayNEWEntryWithSubject:(NSString *)stringSubjectValue body:(NSString *)stringBodyValue hasImage:(BOOL)boolImageValue hasAudioMemo:(BOOL)boolAudioMemoValue isBookmarked:(BOOL)boolBookmarkedValue date:(NSDate *)dateValue dateCreated:(NSDate *)dateCreatedValue;
++ (id)arrayNEWEntryWithSubject:(NSString *)stringSubjectValue body:(NSString *)stringBodyValue hasImage:(BOOL)boolImageValue hasAudioMemo:(BOOL)boolAudioMemoValue isBookmarked:(BOOL)boolBookmarkedValue date:(NSDate *)dateValue dateCreated:(NSDate *)dateCreatedValue index:(NSDictionary *)dicIndex;
+- (NSString *)objectEntry_subject;
+- (NSString *)objectEntry_body;
+- (BOOL)objectEntry_hasImage;
+- (BOOL)objectEntry_hasAudioMemo;
+- (BOOL)objectEntry_isBookmarked;
+- (NSDate *)objectEntry_date;
+- (NSDate *)objectEntry_dateCreated;
+
+@end
+
+static const int SQL_ENTRIES_id;
+static const int SQL_ENTRIES_diaryID;
+static const int SQL_ENTRIES_subject;
+static const int SQL_ENTRIES_body;
+static const int SQL_ENTRIES_hasImage;
+static const int SQL_ENTRIES_hasAudioMemo;
+static const int SQL_ENTRIES_isBookmarked;
+static const int SQL_ENTRIES_date;
+static const int SQL_ENTRIES_dateCreated;
+
+/**
+ * From the parameter list, an array is produced in Entry format
+ * @param [in] statement incoming value from previous call SQLSTatementStep(..)
+ * @return NSArray: Entry format
+ */
+static inline NSMutableArray * SQLStatementRowIntoEntryEntry( sqlite3_stmt *statement) {
+    NSString *stringSubject = [NSString stringWithUTF8String: (char *) sqlite3_column_text( statement, SQL_ENTRIES_subject)];
+    NSString *stringBody = [NSString stringWithUTF8String: (char *) sqlite3_column_text( statement, SQL_ENTRIES_body)];
+    BOOL hasImage = sqlite3_column_int( statement, SQL_ENTRIES_hasImage);
+    BOOL hasAudioMemo = sqlite3_column_int( statement, SQL_ENTRIES_hasAudioMemo);
+    BOOL isBookmarked = sqlite3_column_int( statement, SQL_ENTRIES_isBookmarked);
+    
+    static ISO8601DateFormatter *dateFormatter = nil;
+    if (!dateFormatter)
+        dateFormatter = [[ISO8601DateFormatter alloc] init];
+    [dateFormatter setIncludeTime: YES];
+    
+    NSDate *date = [dateFormatter dateFromString: [NSString stringWithUTF8String: (char *) sqlite3_column_text( statement, SQL_ENTRIES_date)]];
+    NSDate *dateCreated = [dateFormatter dateFromString: [NSString stringWithUTF8String: (char *) sqlite3_column_text( statement, SQL_ENTRIES_dateCreated)]];
+    
+    return [NSMutableArray arrayNEWEntryWithSubject: stringSubject body: stringBody hasImage: hasImage hasAudioMemo: hasAudioMemo isBookmarked: isBookmarked date: date dateCreated: dateCreated index: @{@"id": [NSNumber numberWithInt: sqlite3_column_int( statement, SQL_ENTRIES_id)], @"diaryID": [NSNumber numberWithInt: sqlite3_column_int( statement, SQL_ENTRIES_diaryID)]}];
+    
+};
+
+@interface UniversalFunctions (SQL_Entries_)
+
+/**
+ * Inserts a row to the table Entries
+ * @param [in] arrayEntry: Entries
+ */
++ (void)SQL_ENTRIES_voidInsertRowWithArray:(const NSArray *)arrayEntry;
+/**
+ * Updates an exisiting row to the table Entries
+ * @param [in] arrayEntry: Entries
+ */
++ (void)SQL_ENTRIES_voidUpdateRowForArray:(const NSArray *)arrayEntry;
+/**
+ * Deletes a row to the table Entries
+ * @param [in] arrayEntry: Entries
+ */
++ (void)SQL_ENTRIES_voidDeleteRowWithArray:(const NSArray *)arrayEntry;
+
+@end
--- iLog/DiaryController.m
+++ iLog/DiaryController.m
@@ -114,7 +114,7 @@ static const int SQL_DIARY_dateCreated = 2;
 + (void)SQL_DIARIES_voidDeleteRowWithArray:(const NSArray *)arrayEntry {
     sqlite3 *database;
     if ([UniversalFunctions SQL_returnStatusOfTable: CTSQLDiaries withDatabase: &database]) {
-        NSString *sqlStatement = [NSString stringWithFormat: @"DELETE FROM Diaries where id = %lu;", (unsigned long)[[[arrayEntry index] objectForKey: @"index"] integerValue]];
+        NSString *sqlStatement = [NSString stringWithFormat: @"DELETE FROM Diaries where id = %lu;", (unsigned long)[[[arrayEntry index] objectForKey: @"id"] integerValue]];
         char *err;
         if (!SQLQueryMake( database, sqlStatement, &err)) {
             NSAssert( 0, [NSString stringWithUTF8String: err]);
@@ -130,6 +130,98 @@ static const int SQL_DIARY_dateCreated = 2;
         
     }
     
+}
+
+@end
+
+
+static const NSUInteger ENTRIES_subject = 0;
+static const NSUInteger ENTRIES_body = 1;
+static const NSUInteger ENTRIES_hasImage = 2;
+static const NSUInteger ENTRIES_hasAudioMemo = 3;
+static const NSUInteger ENTRIES_isBookmarked = 4;
+static const NSUInteger ENTRIES_date = 5;
+static const NSUInteger ENTRIES_dateCreated = 6;
+
+@implementation NSArray (ARRAY_Entries_)
+
++ (id)arrayNEWEntry {
+    return [NSMutableArray arrayNEWEntryWithSubject: @"" body: @""];
+    
+}
+
++ (id)arrayNEWEntryWithSubject:(NSString *)stringSubjectValue body:(NSString *)stringBodyValue {
+    return [NSMutableArray arrayNEWEntryWithSubject: stringSubjectValue body: stringBodyValue hasImage: NO hasAudioMemo: NO isBookmarked: NO date: [NSDate date] dateCreated: [NSDate date]];
+    
+}
+
++ (id)arrayNEWEntryWithSubject:(NSString *)stringSubjectValue body:(NSString *)stringBodyValue hasImage:(BOOL)boolImageValue hasAudioMemo:(BOOL)boolAudioMemoValue isBookmarked:(BOOL)boolBookmarkedValue date:(NSDate *)dateValue dateCreated:(NSDate *)dateCreatedValue {
+    return [NSMutableArray arrayNEWEntryWithSubject: stringSubjectValue body: stringBodyValue hasImage: boolImageValue hasAudioMemo: boolAudioMemoValue isBookmarked: boolBookmarkedValue date: dateValue dateCreated: dateCreatedValue index: @{}];
+    
+}
+
++ (id)arrayNEWEntryWithSubject:(NSString *)stringSubjectValue body:(NSString *)stringBodyValue hasImage:(BOOL)boolImageValue hasAudioMemo:(BOOL)boolAudioMemoValue isBookmarked:(BOOL)boolBookmarkedValue date:(NSDate *)dateValue dateCreated:(NSDate *)dateCreatedValue index:(NSDictionary *)dicIndex {
+    return [NSMutableArray arrayWithObjects: stringSubjectValue, stringBodyValue, [NSNumber numberWithBool: boolImageValue], [NSNumber numberWithBool: boolAudioMemoValue], [NSNumber numberWithBool: boolBookmarkedValue], dateValue, dateCreatedValue, dicIndex, nil];
+    
+}
+
+- (NSString *)objectEntry_subject {
+    return [self objectAtIndex: ENTRIES_subject];
+    
+}
+
+- (NSString *)objectEntry_body {
+    return [self objectAtIndex: ENTRIES_body];
+    
+}
+
+- (BOOL)objectEntry_hasImage {
+    return [[self objectAtIndex: ENTRIES_hasImage] boolValue];
+    
+}
+
+- (BOOL)objectEntry_hasAudioMemo {
+    return [[self objectAtIndex: ENTRIES_hasAudioMemo] boolValue];
+    
+}
+
+- (BOOL)objectEntry_isBookmarked {
+    return [[self objectAtIndex: ENTRIES_isBookmarked] boolValue];
+    
+}
+
+- (NSDate *)objectEntry_date {
+    return [self objectAtIndex: ENTRIES_date];
+    
+}
+
+- (NSDate *)objectEntry_dateCreated {
+    return [self objectAtIndex: ENTRIES_date];
+    
+}
+
+
+@end
+
+static const int SQL_ENTRIES_id = 0;
+static const int SQL_ENTRIES_diaryID = 1;
+static const int SQL_ENTRIES_subject = 2;
+static const int SQL_ENTRIES_body = 3;
+static const int SQL_ENTRIES_hasImage = 4;
+static const int SQL_ENTRIES_hasAudioMemo = 5;
+static const int SQL_ENTRIES_isBookmarked = 6;
+static const int SQL_ENTRIES_date = 7;
+static const int SQL_ENTRIES_dateCreated = 8;
+
+@implementation UniversalFunctions (SQL_Entries_)
+
++ (void)SQL_ENTRIES_voidInsertRowWithArray:(const NSArray *)arrayEntry {
+}
+
++ (void)SQL_ENTRIES_voidUpdateRowForArray:(const NSArray *)arrayEntry {
+}
+
++ (void)SQL_ENTRIES_voidDeleteRowWithArray:(const NSArray *)arrayEntry {
 }
 
 @end
