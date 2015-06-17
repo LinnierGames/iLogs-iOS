0a97da7667f91e29953da9a12cab4177496ee776
Erick <esericksanc@gmail.com>
2015-06-16 21:51:02 -0700

    init UniversalFunctions (SQL_Diaries_)

--- iLog/DiaryController.h
+++ iLog/DiaryController.h
@@ -11,3 +11,65 @@
 @interface DiaryController : NSObject
 
 @end
+
+@interface NSArray (ARRAY_)
+
+- (NSDictionary *)index;
+
+@end
+
+const NSUInteger DIARY_title;
+const NSUInteger DIARY_dateCreated;
+
+@interface NSArray (ARRAY_Diaries_)
+
++ (id)arrayNEWDiary;
++ (id)arrayNEWDiaryWithTitle:(NSString *)stringTitleValue dateCreated:(NSDate *)dateCreatedValue;
++ (id)arrayNEWDiaryWithTitle:(NSString *)stringTitleValue dateCreated:(NSDate *)dateCreatedValue index:(NSDictionary *)dicIndex;
+- (NSString *)objectDiary_title;
+- (NSDate *)objectDiary_dateCreated;
+
+@end
+
+const int SQL_DIARY_id;
+const int SQL_DIARY_title;
+const int SQL_DIARY_dateCreated;
+
+/**
+ * From the parameter list, an array is produced in Diary format
+ * @param [in] statement incoming value from previous call SQLSTatementStep(..)
+ * @return NSArray: Diary format
+ */
+static inline NSMutableArray * SQLStatementRowIntoDiaryEntry( sqlite3_stmt *statement) {
+    NSString *stringTitle = [NSString stringWithUTF8String: (char *) sqlite3_column_text( statement, SQL_DIARY_title)];
+    
+    static ISO8601DateFormatter *dateFormatter = nil;
+    if (!dateFormatter)
+        dateFormatter = [[ISO8601DateFormatter alloc] init];
+    [dateFormatter setIncludeTime: YES];
+    
+    NSDate *dateCreated = [dateFormatter dateFromString: [NSString stringWithUTF8String: (char *) sqlite3_column_text( statement, SQL_DIARY_dateCreated)]];
+    
+    return [NSMutableArray arrayNEWDiaryWithTitle: stringTitle dateCreated: dateCreated index: @{@"index": [NSNumber numberWithInt: sqlite3_column_int( statement, SQL_DIARY_id)]}];
+    
+};
+
+@interface UniversalFunctions (SQL_Diaries_)
+
+/**
+ * Inserts a row to the table Diaries
+ * @param [in] arrayEntry: Diaries
+ */
++ (void)SQL_DIARIES_voidInsertRowWithArray:(const NSArray *)arrayEntry;
+/**
+ * Updates an exisiting row to the table Diaries
+ * @param [in] arrayEntry: Diaries
+ */
++ (void)SQL_DIARIES_voidUpdateRowForArray:(const NSArray *)arrayEntry;
+/**
+ * Deletes a row to the table Diaries
+ * @param [in] arrayEntry: Diaries
+ */
++ (void)SQL_DIARIES_voidDeleteRowWithArray:(const NSArray *)arrayEntry;
+
+@end
--- iLog/DiaryController.m
+++ iLog/DiaryController.m
@@ -11,3 +11,86 @@
 @implementation DiaryController
 
 @end
+
+@implementation NSArray (ARRAY_)
+
+- (NSDictionary *)index {
+    return (NSDictionary *)[self lastObject];
+    
+}
+
+@end
+
+const NSUInteger DIARY_title = 0;
+const NSUInteger DIARY_dateCreated = 1;
+
+@implementation NSArray (ARRAY_Diaries_)
+
++ (id)arrayNEWDiary {
+    return [NSMutableArray arrayNEWDiaryWithTitle: @"Untitled" dateCreated: [NSDate date]];
+    
+}
+
++ (id)arrayNEWDiaryWithTitle:(NSString *)stringTitleValue dateCreated:(NSDate *)dateCreatedValue {
+    return [NSMutableArray arrayNEWDiaryWithTitle: stringTitleValue dateCreated: dateCreatedValue index: @{}];
+    
+}
+
++ (id)arrayNEWDiaryWithTitle:(NSString *)stringTitleValue dateCreated:(NSDate *)dateCreatedValue index:(NSDictionary *)dicIndex {
+    return [NSMutableArray arrayWithObjects: stringTitleValue, dateCreatedValue, dicIndex, nil];
+    
+}
+
+- (NSString *)objectDiary_title {
+    return [self objectAtIndex: DIARY_title];
+    
+}
+
+- (NSDate *)objectDiary_dateCreated {
+    return [self objectAtIndex: DIARY_dateCreated];
+    
+}
+
+@end
+
+const int SQL_DIARY_id = 0;
+const int SQL_DIARY_title = 1;
+const int SQL_DIARY_dateCreated = 2;
+
+@implementation UniversalFunctions (SQL_Diaries_)
+
++ (void)SQL_DIARIES_voidInsertRowWithArray:(const NSArray *)arrayEntry {
+    sqlite3 *database;
+    if ([UniversalFunctions SQL_returnStatusOfTable: CTSQLDiaries withDatabase: &database]) {
+        static ISO8601DateFormatter *dateFormatter = nil;
+        if (!dateFormatter)
+            dateFormatter = [[ISO8601DateFormatter alloc] init];
+        [dateFormatter setIncludeTime: YES];
+        
+        NSString *sqlStatement = [NSString stringWithFormat: @"INSERT INTO Diaries (title, dateCreated) values (\"%@\", \"%@\");", [arrayEntry objectDiary_title], [dateFormatter stringFromDate: [arrayEntry objectDiary_dateCreated]]];
+        char *err;
+        if (!SQLQueryMake( database, sqlStatement, &err)) {
+            NSAssert( 0, [NSString stringWithUTF8String: err]);
+            sqlite3_close( database);
+            NSLog( @"***Failed to Add to Table: +SQL_ACTIVITY_voidInsertRowWithArray:");
+            
+        } else
+            NSLog( @"Added to Table: %@: +SQL_ACTIVITY_voidInsertRowWithArray:", arrayEntry);
+        
+    } else {
+        [UniversalFunctions SQL_voidCreateTable: CTSQLDiaries];
+        [UniversalFunctions SQL_DIARIES_voidInsertRowWithArray: arrayEntry];
+        
+    }
+    
+}
+
++ (void)SQL_DIARIES_voidUpdateRowForArray:(const NSArray *)arrayEntry {
+    
+}
+
++ (void)SQL_DIARIES_voidDeleteRowWithArray:(const NSArray *)arrayEntry {
+    
+}
+
+@end
