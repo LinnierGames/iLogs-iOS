e4e26dc31382061bc7d8ebcc702925cfdade3c75
Erick <esericksanc@gmail.com>
2017-01-11 11:27:24 -0800

    extend Universal Functions for DIARY_CoreData_

---
+++ iLog/DiaryController+CoreData.h
@@ -0,0 +1,13 @@
+//
+//  DiaryController+CoreData.h
+//  iLogs - Universal iOS
+//
+//  Created by Erick Sanchez on 1/11/17.
+//  Copyright © 2017 Erick Sanchez. All rights reserved.
+//
+
+#import "Story+CoreDataClass.h"
+
+@interface UniversalFunctions (DIARY_CoreData_)
+
+@end
---
+++ iLog/DiaryController+CoreData.m
@@ -0,0 +1,13 @@
+//
+//  DiaryController+CoreData.m
+//  iLogs - Universal iOS
+//
+//  Created by Erick Sanchez on 1/11/17.
+//  Copyright © 2017 Erick Sanchez. All rights reserved.
+//
+
+#import "DiaryController+CoreData.h"
+
+@implementation UniversalFunctions (DIARY_CoreData_)
+
+@end
